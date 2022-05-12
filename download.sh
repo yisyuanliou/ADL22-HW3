b4e2b64b14b1057ba9fc5f1b7ba912861268c1f8
Yi-Syuan Liou <y002783@gmail.com>
2022-05-13 05:18:24 +0800

    Add bash script

---
+++ download.sh
@@ -0,0 +1,2 @@
+wget -r https://www.dropbox.com/s/skxdsbmcbsgez8n/mt5.zip?dl=1 -O mt5.zip
+unzip mt5.zip
---
+++ run.sh
@@ -0,0 +1,2 @@
+python3.8 predict.py --file_path "${1}" --output_path "${2}"
+python3.8 eval.py -r "${1}" -s "${2}"
